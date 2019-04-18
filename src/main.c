fd37226701a2289af682aeeb93460a38fb90073e
Jesper Derander <jesper.derander@endian.se>
2019-04-18 17:26:40 +0200

    Removed printk statements in favor of using the log module instead

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 #include <stddef.h>
 #include <errno.h>
 #include <zephyr.h>
-#include <misc/printk.h>
+#include <logging/log.h>
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
@@ -20,20 +20,24 @@
 #include <net/net_if.h>
 #include <net/bt.h>
 
+LOG_MODULE_REGISTER(IPSP_ROUTER_CONNECT_SAMPLE, LOG_LEVEL_INF);
+
+#define BT_CONNECTED 1
+#define BT_DISCONNECTED 0
+
 /* Will attempt to connect to this bluetooth address */
-#define PEER_BT_ADDRESS "b8:27:eb:8e:32:17"
+#define PEER_BT_ADDRESS "00:1a:7d:da:71:0b"
 
 #define BT_THREAD_STACKSIZE 1024
 K_THREAD_STACK_DEFINE(bt_thread_stack, BT_THREAD_STACKSIZE);
 
 static struct k_thread bt_thread_data;
 
-static int connected;
-
 static struct net_if *interface;
 
 static struct in6_addr *ip6_address;
 
+static u8_t bt_conn_status;
 
 static void check_device(const bt_addr_le_t *addr, s8_t rssi, u8_t type,
 			 struct net_buf_simple *ad)
@@ -53,63 +57,81 @@ static void check_device(const bt_addr_le_t *addr, s8_t rssi, u8_t type,
     strncpy(dev_addr_format, dev_addr, sizeof(PEER_BT_ADDRESS)); 
 	dev_addr_format[sizeof(PEER_BT_ADDRESS)-1] = '\0';
 
-	printk("Found device: %s\n",dev_addr);
+	LOG_INF("Found device: %s",log_strdup(dev_addr));
 
-    /* Check if the device is what we are lloking for */
+    /* Check if the device is what we are looking for */
     if(strcmp(dev_addr_format, PEER_BT_ADDRESS)!=0){
         return;
     }
-	printk("Device with address %s was found\n", PEER_BT_ADDRESS);
+	LOG_INF("Device with address %s was found", PEER_BT_ADDRESS);
 
     /* Stop the scan */
 	if (bt_le_scan_stop()) {
 		return;
 	}
 
-    printk("Attemting to connect to %s\n", dev_addr_format);
+    LOG_INF("Attemting to connect to %s", log_strdup(dev_addr_format));
 	net_mgmt(NET_REQUEST_BT_CONNECT, interface, (void*)addr, sizeof(*addr));
-	connected = 1;
 }
 
 static void bt_scan(void){
-	printk("Scanning for connectable device with address %s\n", PEER_BT_ADDRESS);
+	LOG_INF("Scanning for connectable device with address %s", PEER_BT_ADDRESS);
 
-    /* Begin scanning for connectable devices */ 
+    /* Begin scanning for connectable devices */
 	int ret = bt_le_scan_start(BT_LE_SCAN_ACTIVE, check_device);
 	if (ret) {
-		printk("Scanning failed to start, bt_le_scan_start returned %d\n", ret);
+		LOG_INF("Scanning failed to start, bt_le_scan_start returned %d", ret);
 		return;
 	}
 }
 
+static void connected(struct bt_conn *conn, u8_t err)
+{
+	LOG_INF("Bluetooth connection established, waiting for ipv6 address");
+	bt_conn_status = BT_CONNECTED;
+}
 
+static void disconnected(struct bt_conn *conn, u8_t reason){
+	LOG_INF("Bluetooth disconnected");
+	bt_conn_status = BT_DISCONNECTED;
+}
+
+static struct bt_conn_cb conn_callbacks = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
 
 void main(void)
 {
-	connected = 0;
+	bt_conn_status = BT_DISCONNECTED;
 
 	int ret = bt_enable(NULL);
+
 	if (ret) {
-		printk("Bluetooth could not be initialized, bt_enable returned %d\n", ret);
+		LOG_INF("Bluetooth could not be initialized, bt_enable returned %d", ret);
 		return;
 	}
 
-	printk("Bluetooth initialized\n");
+	LOG_INF("Bluetooth initialized");
 
+	bt_conn_cb_register(&conn_callbacks);
 
 	/* Get the default interface in use */ 
 	interface = net_if_get_default();
 
 	if(!interface){
-		printk("Interface is NULL!\n");
+		LOG_INF("Interface is NULL!");
 	}
 
+	/* Initiate ble scan on separate thread */
 	k_thread_create(&bt_thread_data, bt_thread_stack, BT_THREAD_STACKSIZE,
 			(k_thread_entry_t)bt_scan,
 			NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);	
 
-	while(1){
-		if(connected){
+	while(1)
+	{
+		if(bt_conn_status == BT_CONNECTED)
+		{
 			/* check if there is an assigned ipv6 global address */
 			ip6_address = net_if_ipv6_get_global_addr(&interface);
 			if(ip6_address){
@@ -118,12 +140,14 @@ void main(void)
 				/* Convert ipv6 address to string */
 				net_addr_ntop(AF_INET6, ip6_address, ip6_address_str, sizeof(ip6_address_str));
 			
-				printk("The interface has been assigned the global address %s", ip6_address_str);
+				LOG_INF("The interface has been assigned the global address %s", log_strdup(ip6_address_str));
 				break;
 			}
-			
-			
 		}
 		k_sleep(500);
+	}
+	
+	while(1){
+		k_sleep(100000);
 	}
 }
